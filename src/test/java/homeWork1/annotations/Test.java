c1072031302f96c27b4d069025235fd3b00305ff
Максим <max.kushtanov@gmail.com>
2025-09-12 13:39:39 +0300

    Домашнее задание №3

--- src/test/java/homeWork1/annotations/Test.java
+++ src/test/java/homeWork1/annotations/Test.java
@@ -1,4 +1,4 @@
-package annotations;
+package homeWork1.annotations;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -7,5 +7,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-public @interface BeforeSuite {
+public @interface Test {
+    int priority() default 5;
 }
